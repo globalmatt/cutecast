dde63f5e02ef116b43127fc50649017a34ef7ee2
Matt Doyle <matt@elated.com>
2022-01-02 19:16:35 +1100

    Improve `getConditionName()` test coverage
    #4

--- src/utilities/getConditionName.test.js
+++ src/utilities/getConditionName.test.js
@@ -1,6 +1,6 @@
 import getConditionName from "./getConditionName";
 
-test("Returns a condition name", () => {
+test("Returns a daytime condition name", () => {
     expect(
         getConditionName(
             800, // conditionCode
@@ -9,4 +9,15 @@ test("Returns a condition name", () => {
             43200 // todaySunsetUTC
         )
     ).toBe("Sunny");
+});
+
+test("Returns a nighttime condition name", () => {
+    expect(
+        getConditionName(
+            800, // conditionCode
+            43201, // timeUTC
+            1, // todaySunriseUTC,
+            43200 // todaySunsetUTC
+        )
+    ).toBe("Clear");
 });
